58724a122be27ba1da2c56fb351a16084c43cec1
Loren Wilks <Loren.wilks@gmail.com>
2022-09-06 10:52:31 -0600

    commit tree 534c5ae669246c4153742ebc3c806605fbb62c4f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["fiction", "non-fiction"]}
+    validate :clickbait
+
+def clickbait
+if title.include?("Won't Believe")
+errors.add(:title, "must be clickbait")    
+end
+if title.include?("Secret")
+    errors.add(:title, "must be clickbait")    
+    end
+if title.include?("Top")
+    errors.add(:title, "must be clickbait")    
+    end
+if title.include?("Guess")
+    errors.add(:title, "must be clickbait")    
+    end
+end
 end
